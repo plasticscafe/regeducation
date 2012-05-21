4763776e0a2b32613e2129c2ebfc7be65c148b8f
Naoyuki ABE <plasticscafe@gmail.com>
2012-05-21 20:55:03 +0900

    is_equal test

---
+++ js/test/common_06_RegexIsEqual.coffee
@@ -0,0 +1,28 @@
+should = require 'should'
+fs = require 'fs'
+
+eval fs.readFileSync 'common.js', 'utf-8'
+
+describe 'RegexCheck', ->
+  regex = new Regex
+  describe 'basic test', ->
+    it 'defined is_equal', ->
+      regex.is_equal.should.ok
+  describe 'item equals', ->
+    it 'scalar eql', ->
+      regex.is_equal('hoge', 'hoge').should.true
+    it 'scalar not equal', ->
+      regex.is_equal('hoge', 'hoge1').should.false
+  describe 'array equals', ->
+    it '1 item eql', ->
+      regex.is_equal(['hoge'], ['hoge']).should.true
+    it '1 item not equal', ->
+      regex.is_equal(['hoge'], ['hoge1']).should.false
+    it '2 item eql', ->
+      regex.is_equal(['hoge', 'huga'], ['hoge', 'huga']).should.true
+    it '2 item not equal', ->
+      regex.is_equal(['hoge', 'huga'], ['hoge', 'hige']).should.false
+    it 'item length not equal', ->
+      regex.is_equal(['hoge', 'huga'], ['hoge']).should.false
+    it 'item length not equal 2', ->
+      regex.is_equal(['hoge'], ['hoge', 'hige']).should.false
---
+++ js/test/common_06_RegexIsEqual.js
@@ -0,0 +1,45 @@
+var fs, should;
+
+should = require('should');
+
+fs = require('fs');
+
+eval(fs.readFileSync('common.js', 'utf-8'));
+
+describe('RegexCheck', function() {
+  var regex;
+  regex = new Regex;
+  describe('basic test', function() {
+    return it('defined is_equal', function() {
+      return regex.is_equal.should.ok;
+    });
+  });
+  describe('item equals', function() {
+    it('scalar eql', function() {
+      return regex.is_equal('hoge', 'hoge').should["true"];
+    });
+    return it('scalar not equal', function() {
+      return regex.is_equal('hoge', 'hoge1').should["false"];
+    });
+  });
+  return describe('array equals', function() {
+    it('1 item eql', function() {
+      return regex.is_equal(['hoge'], ['hoge']).should["true"];
+    });
+    it('1 item not equal', function() {
+      return regex.is_equal(['hoge'], ['hoge1']).should["false"];
+    });
+    it('2 item eql', function() {
+      return regex.is_equal(['hoge', 'huga'], ['hoge', 'huga']).should["true"];
+    });
+    it('2 item not equal', function() {
+      return regex.is_equal(['hoge', 'huga'], ['hoge', 'hige']).should["false"];
+    });
+    it('item length not equal', function() {
+      return regex.is_equal(['hoge', 'huga'], ['hoge']).should["false"];
+    });
+    return it('item length not equal 2', function() {
+      return regex.is_equal(['hoge'], ['hoge', 'hige']).should["false"];
+    });
+  });
+});
