f5ea154cd648f18ed8ee046ffd8a81866a4267a8
Naoyuki ABE <plasticscafe@gmail.com>
2012-05-21 20:55:40 +0900

    matched array test

--- js/test/common_05_RegexCheck.coffee
+++ js/test/common_05_RegexCheck.coffee
@@ -10,22 +10,50 @@ describe 'RegexCheck', ->
       regex.check.should.be.ok
   describe 'call RegexExec', ->
     it 'check regex test', ->
-      regex.check('/abc/', 'abc1ABC2abc', true).should.be.true
+      regex.check('/abc/', 'abc1ABC2abc', true).should.eql(
+        is_success: true, result: true 
+      )
     it 'check regex test fail', ->
-      regex.check('/abcd/', 'abc1ABC2abc', true).should.be.false
+      regex.check('/abcd/', 'abc1ABC2abc', true).should.eql(
+        is_success: false, result: false
+      )
+    it 'check regex test fail 2', ->
+      regex.check('/abcd/', 'abc1ABC2abc', false).should.eql(
+        is_success: true, result: false
+      )
     it 'check regex replace', ->
-      regex.check('s/abc/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.be.true
+      regex.check('s/abc/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.eql(
+        is_success: true, result: 'hoge1ABC2abc'
+      )
     it 'check regex replace fail', ->
-      regex.check('s/abcd/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.be.false
+      regex.check('s/abcd/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.eql(
+        is_success: false, result: 'abc1ABC2abc'
+      )
     it 'check regex match', ->
-      regex.check('m/abc/', 'abc1ABC2abc', ['abc']).should.be.true
+      regex.check('m/abc/', 'abc1ABC2abc', ['abc']).should.eql(
+        is_success: true, result: ['abc']
+      )
     it 'check regex match false', ->
-      regex.check('m/abcd/', 'abc1ABC2abc', ['abc']).should.be.false
+      regex.check('m/abcd/', 'abc1ABC2abc', ['abc']).should.eql(
+        is_success: false, result: null
+      )
+    it 'check regex match false 2', ->
+      regex.check('m/abc/', 'abc1ABC2abc', ['abc', 'abc']).should.eql(
+        is_success: false, result: ['abc']
+      )
     it 'check regex match 2 items', ->
-      regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'abc']).should.be.true
+      regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'abc']).should.eql(
+        is_success: true, result: ['abc', 'abc']
+      )
     it 'check regex match 2 items false', ->
-      regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'ebc']).should.be.false
+      regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'ebc']).should.eql(
+        is_success: false, result: ['abc', 'abc']
+      )
     it 'check regex match 3 items', ->
-      regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ABC', 'abc']).should.be.true
+      regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ABC', 'abc']).should.eql(
+        is_success: true, result: ['abc', 'ABC','abc']
+      )
     it 'check regex match 3 items false', ->
-      regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ebc']).should.be.false
+      regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ebc']).should.eql(
+        is_success: false, result: ['abc', 'ABC','abc']
+      )
--- js/test/common_05_RegexCheck.js
+++ js/test/common_05_RegexCheck.js
@@ -16,34 +16,76 @@ describe('RegexCheck', function() {
   });
   return describe('call RegexExec', function() {
     it('check regex test', function() {
-      return regex.check('/abc/', 'abc1ABC2abc', true).should.be["true"];
+      return regex.check('/abc/', 'abc1ABC2abc', true).should.eql({
+        is_success: true,
+        result: true
+      });
     });
     it('check regex test fail', function() {
-      return regex.check('/abcd/', 'abc1ABC2abc', true).should.be["false"];
+      return regex.check('/abcd/', 'abc1ABC2abc', true).should.eql({
+        is_success: false,
+        result: false
+      });
+    });
+    it('check regex test fail 2', function() {
+      return regex.check('/abcd/', 'abc1ABC2abc', false).should.eql({
+        is_success: true,
+        result: false
+      });
     });
     it('check regex replace', function() {
-      return regex.check('s/abc/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.be["true"];
+      return regex.check('s/abc/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.eql({
+        is_success: true,
+        result: 'hoge1ABC2abc'
+      });
     });
     it('check regex replace fail', function() {
-      return regex.check('s/abcd/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.be["false"];
+      return regex.check('s/abcd/hoge/', 'abc1ABC2abc', 'hoge1ABC2abc').should.eql({
+        is_success: false,
+        result: 'abc1ABC2abc'
+      });
     });
     it('check regex match', function() {
-      return regex.check('m/abc/', 'abc1ABC2abc', ['abc']).should.be["true"];
+      return regex.check('m/abc/', 'abc1ABC2abc', ['abc']).should.eql({
+        is_success: true,
+        result: ['abc']
+      });
     });
     it('check regex match false', function() {
-      return regex.check('m/abcd/', 'abc1ABC2abc', ['abc']).should.be["false"];
+      return regex.check('m/abcd/', 'abc1ABC2abc', ['abc']).should.eql({
+        is_success: false,
+        result: null
+      });
+    });
+    it('check regex match false 2', function() {
+      return regex.check('m/abc/', 'abc1ABC2abc', ['abc', 'abc']).should.eql({
+        is_success: false,
+        result: ['abc']
+      });
     });
     it('check regex match 2 items', function() {
-      return regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'abc']).should.be["true"];
+      return regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'abc']).should.eql({
+        is_success: true,
+        result: ['abc', 'abc']
+      });
     });
     it('check regex match 2 items false', function() {
-      return regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'ebc']).should.be["false"];
+      return regex.check('m/abc/g', 'abc1ABC2abc', ['abc', 'ebc']).should.eql({
+        is_success: false,
+        result: ['abc', 'abc']
+      });
     });
     it('check regex match 3 items', function() {
-      return regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ABC', 'abc']).should.be["true"];
+      return regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ABC', 'abc']).should.eql({
+        is_success: true,
+        result: ['abc', 'ABC', 'abc']
+      });
     });
     return it('check regex match 3 items false', function() {
-      return regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ebc']).should.be["false"];
+      return regex.check('m/abc/gi', 'abc1ABC2abc', ['abc', 'ebc']).should.eql({
+        is_success: false,
+        result: ['abc', 'ABC', 'abc']
+      });
     });
   });
 });
